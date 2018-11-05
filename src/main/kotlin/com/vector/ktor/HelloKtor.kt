3e73db7589a9b978153b1dd735174dec8b408258
vector.huang <642378415@qq.com>
2018-11-05 22:58:05 +0800

    init

---
+++ src/main/kotlin/com/vector/ktor/HelloKtor.kt
@@ -0,0 +1,27 @@
+package com.vector.ktor
+
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+/**
+ * @author: vector.huang
+ * @date: 2018/11/05 22:27
+ */
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, port = 8081) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!", ContentType.Text.Plain)
+            }
+            get("/demo") {
+                call.respondText("HELLO WORLD!")
+            }
+        }
+    }
+    server.start(wait = true)
+}
